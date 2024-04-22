b1ff3b9476945fcb46c2bda21caa919633858015
Antti <antti.saaristo@gmail.com>
2024-04-22 23:56:43 +0300

    added lsp log config example

--- lua/custom/plugins/robotframework.lua
+++ lua/custom/plugins/robotframework.lua
@@ -2,9 +2,13 @@
 --
 return {
     require('lspconfig').robotframework_ls.setup{
-
+        
+        -- cmd = { "robotframework_ls", "-vv", "--log-file=/tmp/logfile.log" },
         settings = {
             robot = {
+                pythonpath = {
+                        "/Users/anttisaaristo/tmp/robot/ide-test/library"
+                    },
                 -- Enable linting
                 lint = {
                     robocop = {
