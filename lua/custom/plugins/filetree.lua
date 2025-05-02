e5917847217769c3c70ce23ba4f342729eea60c3
Antti Saaristo <antti.saaristo@rebase.fi>
2025-05-02 14:57:08 +0300

    Neo-tree close window, not toggle and show hidden files

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,10 +10,18 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Show filtered (hidden) items by default
+          hide_dotfiles = false, -- Show dotfiles (e.g., .git, .env)
+          hide_gitignored = false, -- Show files ignored by .gitignore
+        },
+      },
       window = {
         mappings = {
           -- Add your mapping here:
-          ['<C-b>'] = 'toggle', -- Use the "toggle" command provided by Neo-tree
+          -- ['q'] = 'close_window', -- instead of custom logic or :q
+          ['<C-b>'] = 'close_window', -- Use the "toggle" command provided by Neo-tree
           -- Or, you might prefer it to only close when focused:
           -- ["<C-b>"] = "close_window",
 
